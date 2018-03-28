83072e33c4cefe6fc74476dbf5788d46f389bbe1
wangyang <wangyang024@lianjia.com>
2018-03-28 10:52:17 +0800

    测试增加第二个类

---
+++ module1/src/Class2.java
@@ -0,0 +1,13 @@
+/**
+ * Copyright (C), 2018, 链家网
+ * FileName: Class2
+ * Author:   ljadmin
+ * Date:     2018/3/28 10:50
+ * Description: 第二个类
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+public class Class2 {
+}
