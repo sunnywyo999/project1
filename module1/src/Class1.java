744388e59086abd46e866e6e141ebc71ec4edfe5
wangyang <wangyang024@lianjia.com>
2018-03-28 10:45:48 +0800

    测试git提交  1

---
+++ module1/src/Class1.java
@@ -0,0 +1,17 @@
+/**
+ * Copyright (C), 2018, 链家网
+ * FileName: Class1
+ * Author:   ljadmin
+ * Date:     2018/3/27 18:18
+ * Description: 第一个类
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+public class Class1 {
+    public static void main(String[] args) {
+        System.out.println("one");
+    }
+
+}
---
+++ module2/src/Class2.java
@@ -0,0 +1,14 @@
+/**
+ * Copyright (C), 2018, 链家网
+ * FileName: Class2
+ * Author:   ljadmin
+ * Date:     2018/3/27 18:19
+ * Description: 第二个类
+ * History:
+ * <author>          <time>          <version>          <desc>
+ * 作者姓名           修改时间           版本号              描述
+ */
+
+public class Class2  {
+
+}
